40e060ddb1281cf3106d9502de832616d3a81131
BrendanCain <126262599+BrendanCain@users.noreply.github.com>
2023-03-16 10:22:47 +0000

    Add files via upload

--- src/example-client.js
+++ src/example-client.js
@@ -1,4 +1,5 @@
 //Solution definitely isn't perfect but I only started learning javascript this morning
+// With more time I would have: got tests working & created more, moved solution to typescript, cleaned up the mapping through creating objects for input & outputs
 
 const BookSearchApiClient = require("./BookSearchApiClient.js");
 
